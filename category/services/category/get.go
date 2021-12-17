d51cdc5a35c2626f688e18185ed9ee081533e2c6
dipto0079 <sudiptobiswas.coder@gmail.com>
2021-12-17 18:30:46 +0600

    com

--- category/services/category/get.go
+++ category/services/category/get.go
@@ -1,29 +1,29 @@
  package category
 
 
-// import (
-// 	//"blog/category/storage"
-// 	tpc "blog/gunk/v1/category"
-// 	"context"
-// 	//"log"
+import (
+	//"blog/category/storage"
+	tpc "blog/gunk/v1/category"
+	"context"
+	//"log"
 
-// 	"google.golang.org/grpc/codes"
-// 	"google.golang.org/grpc/status"
-// )
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
-// func(s *Svc) GetAllData(ctx context.Context,) (*tpc.GetAllDataCategoryResponse, error){
+func(s *Svc) GetAllData(ctx context.Context,) (*tpc.GetAllDataCategoryResponse, error){
 	
-// 	 //category := storage.Category{}
+	 //category := storage.Category{}
 	
-// 	id,err:=s.core.Get_AllData_ser(context.Background())
-// 	if err != nil {
-// 		return nil, status.Errorf(codes.Internal,"failed to create category: %s",err)
-// 	}
-// 	return &tpc.GetAllDataCategoryResponse{
-// 		Category: &tpc.Category{
-// 			ID: id.ID,
-// 			Title: id.Title,
-// 		},
-// 	},nil
+	id,err:=s.core.Get_AllData_ser(context.Background())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal,"failed to create category: %s",err)
+	}
+	return &tpc.GetAllDataCategoryResponse{
+		Category: &tpc.Category{
+			ID: id.ID,
+			Title: id.Title,
+		},
+	},nil
 
-// }
+}
